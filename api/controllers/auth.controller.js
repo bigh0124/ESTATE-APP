3ce921459dd2eac979d730421e3aa3b719fe5534
bigh0124 <thubgi0629@gmail.com>
2024-05-15 16:09:00 +0800

    Feat: 建立signIn api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
 import { createError } from "../utils/createError.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -17,6 +18,39 @@ export const register = async (req, res, next) => {
     });
 
     res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    next(createError());
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    //check if the user exists
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+    if (!user) return next(createError(401, "Invalid Credentials: User NOT found"));
+
+    //check if the password is correct
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) return next(createError(401, "Invalid Credentials: Password NOT correct"));
+
+    //generate cookie token and send to the user
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 86400000,
+        //make sure enable secure on prod
+        //  secure: true
+      })
+      .status(200)
+      .json({ message: "Sign In successfully" });
   } catch (err) {
     next(createError());
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
+import { register, signIn } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/signIn", signIn);
 
 export default router;
