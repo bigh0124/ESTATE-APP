cf2eb9038c6a7cfd8cd9231b26dc5e037776a42e
bigh0124 <thubgi0629@gmail.com>
2024-05-15 14:50:30 +0800

    Feat: 建立 middleware handle error

--- api/app.js
+++ api/app.js
@@ -1,6 +1,24 @@
 import express from "express";
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something went wrong!";
+
+  const error = {
+    success: false,
+    statusCode,
+    message,
+  };
+
+  res.status(statusCode).json(error);
+});
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800");
