7c08a56bb339a728868287adf040fc707253e8a4
bigh0124 <thubgi0629@gmail.com>
2024-05-15 16:08:27 +0800

    Feat: 新增dotenv and cookieParser lib

--- api/app.js
+++ api/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 
