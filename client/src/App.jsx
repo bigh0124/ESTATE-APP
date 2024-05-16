0906fcd6cc591b35e09377a4ac943d174166f647
bigh0124 <thubgi0629@gmail.com>
2024-05-16 16:34:52 +0800

    Feat: 新增react query lib

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ListPage from "./pages/listPage/ListPage";
 import HomePage from "./pages/homePage/HomePage";
 import Layout from "./pages/layout/Layout";
@@ -8,6 +9,8 @@ import Register from "./pages/register/register";
 import Login from "./pages/login/Login";
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -42,7 +45,11 @@ const App = () => {
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
