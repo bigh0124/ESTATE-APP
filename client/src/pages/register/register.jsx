560911d36fff15a32d19b2763ef0ffecb1f9c50d
bigh0124 <thubgi0629@gmail.com>
2024-05-16 16:23:41 +0800

    Feat: handle register api to server

--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -1,16 +1,45 @@
+import axios from "axios";
 import "./register.scss";
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Register = () => {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      await axios.post("http://localhost:8800/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      navigate("/login");
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  };
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button>Register</button>
+          <button type="submit">Register</button>
+          {error && <span>username or email has duplicated</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
