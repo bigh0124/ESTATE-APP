8f73591a643040b6e6191f3cf416101b225cf779
bigh0124 <thubgi0629@gmail.com>
2024-05-17 17:48:43 +0800

    Feat: 新增 updateUser api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { createError } from "../utils/createError.js";
 import prisma from "../lib/prisma.js";
 
 export const getUser = async (req, res, next) => {
-  if (req.userId !== req.params.userId) next(createError(401, "Not Authoriztion"));
+  if (req.userId !== req.params.userId) next(createError(403, "Not Authoriztion"));
 
   try {
     const user = await prisma.user.findUnique({ where: { id: req.userId } });
@@ -11,6 +11,18 @@ export const getUser = async (req, res, next) => {
     const { password, ...userInfo } = user;
 
     res.status(200).json(userInfo);
+  } catch (err) {
+    next(createError());
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.userId !== req.params.userId) next(createError(403, "Not Authoriztion"));
+
+  try {
+    const updatedUser = await prisma.user.update({ where: { id: req.userId }, data: { ...req.body } });
+
+    res.status(200).json(updatedUser);
   } catch (err) {
     next(createError());
   }
