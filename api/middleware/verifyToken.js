6186b93428a7e2da4fd80f3f5cba78cdbf2ea846
bigh0124 <thubgi0629@gmail.com>
2024-05-17 17:31:30 +0800

    Fix: 修正payload

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,15 +1,16 @@
-import { createError } from "../utils/createError";
+import { createError } from "../utils/createError.js";
 import jwt from "jsonwebtoken";
 
 const verify = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token) return next(createError(401, "Unauthorized!"));
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(createError(403, "Forbidden!"));
-    req.user = user;
-  });
 
-  next();
+  if (!token) return next(createError(401, "Unauthorized!"));
+  jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+    if (err) return next(createError(403, "Forbidden!"));
+
+    req.userId = payload.id;
+    next();
+  });
 };
 
 export default verify;
