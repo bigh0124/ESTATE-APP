197c941c4fcbabafc1af9dc88e627177791715b0
bigh0124 <thubgi0629@gmail.com>
2024-05-20 12:47:01 +0800

    Feat: 建立 updatePost api

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -4,7 +4,7 @@ import { createError } from "../utils/createError.js";
 export const getPosts = async (req, res, next) => {
   try {
     const posts = await prisma.post.findMany({ take: 10 });
-    if (!posts) next(createError(404, "posts not found"));
+    if (!posts) return next(createError(404, "posts not found"));
     res.status(200).json(posts);
   } catch (err) {
     next(createError());
@@ -14,7 +14,7 @@ export const getPosts = async (req, res, next) => {
 export const getPost = async (req, res, next) => {
   try {
     const post = await prisma.post.findUnique({ where: { id: req.params.postId } });
-    if (!post) next(createError(404, "post not found"));
+    if (!post) return next(createError(404, "post not found"));
 
     res.status(200).json(post);
   } catch (err) {
@@ -25,7 +25,6 @@ export const getPost = async (req, res, next) => {
 export const addPost = async (req, res, next) => {
   const body = req.body;
   const userId = req.userId;
-  console.log({ ...body, userId });
   try {
     const newPosts = await prisma.post.create({
       data: {
@@ -35,6 +34,26 @@ export const addPost = async (req, res, next) => {
     });
 
     res.status(201).json(newPosts);
+  } catch (err) {
+    next(createError());
+  }
+};
+
+export const updatePost = async (req, res, next) => {
+  const postId = req.params.postId;
+  const userId = req.userId;
+
+  try {
+    const post = await prisma.post.findUnique({ where: { id: postId } });
+    if (!post) return next(createError(404, "post not found"));
+    if (post.userId !== userId) return next(createError(403, "You are only allowed to update your own post."));
+    const updatedPost = await prisma.post.update({
+      where: { id: postId },
+      data: {
+        ...req.body,
+      },
+    });
+    res.status(200).json(updatedPost);
   } catch (err) {
     next(createError());
   }
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addPost, getPost, getPosts } from "../controllers/post.controller.js";
+import { addPost, getPost, getPosts, updatePost } from "../controllers/post.controller.js";
 import verifyToken from "../middleware/verifyToken.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/getPosts", getPosts);
 router.get("/getPost/:postId", getPost);
 router.post("/addPost", verifyToken, addPost);
+router.put("/updatePost/:postId", verifyToken, updatePost);
 
 export default router;
