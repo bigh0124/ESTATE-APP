c317ad8c615a39905c331673a7bb9a7332b24321
bigh0124 <thubgi0629@gmail.com>
2024-05-20 12:22:40 +0800

    Feat: 建立 getPost route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getPosts } from "../controllers/post.controller.js";
+import { getPost, getPosts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/getPosts", getPosts);
+router.get("/getPost/:postId", getPost);
 
 export default router;
