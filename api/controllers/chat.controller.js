3bc9b5628bf2bd495487a6c11fc61b4b0a332074
bigh0124 <thubgi0629@gmail.com>
2024-05-23 13:43:54 +0800

    Feat: 新增getChats api

--- api/controllers/chat.controller.js
+++ api/controllers/chat.controller.js
@@ -1,6 +1,22 @@
 import prisma from "../lib/prisma.js";
 import { createError } from "../utils/createError.js";
 
+export const getChats = async (req, res, next) => {
+  const userId = req.userId;
+  try {
+    const chats = await prisma.chat.findMany({
+      where: {
+        userIDs: {
+          hasSome: [userId],
+        },
+      },
+    });
+    res.status(200).json(chats);
+  } catch (err) {
+    next(createError());
+  }
+};
+
 export const addChat = async (req, res, next) => {
   const userId = req.userId;
   const receiverId = req.body.receiverId;
--- api/routes/chat.route.js
+++ api/routes/chat.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
-import { addChat } from "../controllers/chat.controller.js";
+import { addChat, getChats } from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
+router.get("/getChats", verifyToken, getChats);
 router.post("/addChat", verifyToken, addChat);
 
 export default router;
